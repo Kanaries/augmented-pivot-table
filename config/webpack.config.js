7fc1a5754925a6cd7e50be00782de64b2774f28e
ObservedObserver <270001151@qq.com>
2020-08-03 16:45:49 +0800

    Initial commit

---
+++ config/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "../src/main.tsx"),
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              configFile: path.resolve(__dirname, "./tsconfig.json"),
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "../dist"),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "../dist"),
+    compress: true,
+    port: 1996,
+  },
+};
